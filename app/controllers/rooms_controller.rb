71faf4218ec524792d0938d2be0f58bc60454a66
Hiro-KT <kh01210237.khko@gmail.com>
2021-03-11 16:12:13 +0900

    [Function]Room Create

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,18 @@ class RoomsController < ApplicationController
   def new
     @room = Room.new
   end
+
+  def create
+    @room = Room.new(user_params)
+
+    if @room.save
+      redirect_to room_url
+    else
+      render 'new'
+    end
+  end
+
+  def room_params
+    params.require(:room).permit(:home_type, :room_type, :guest_count, :bedroom_count, :bathroom_count)
+  end
 end
