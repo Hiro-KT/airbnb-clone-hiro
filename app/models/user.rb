f209ee4017b7e72cbb866b193f0ac18838e4088b
Hiro-KT <78471137+Hiro-KT@users.noreply.github.com>
2021-03-11 15:48:31 +0900

    [Layout]Room New (#10)
    * [Layout]Room New
    
    * change form in rooms/new
    
    * add class to form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :lockable, :timeoutable, :trackable 
+  # :lockable, :timeoutable, :trackable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :omniauthable
 
