18ccfd65d04c921b87ba79b32f6e6052a8a92d79
Hiro-KT <kh01210237.khko@gmail.com>
2021-03-16 16:28:47 +0900

    [Function]Publish

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,2 +1,5 @@
 module RoomsHelper
+  def is_ready?(room)
+   !room.photos.nil? && !room.price.nil? && !room.address.nil? && !room.home_type.nil? && !room.room_type.nil? && !room.guest_count.nil? && !room.bedroom_count.nil? && !room.bathroom_count.nil?
+  end
 end
